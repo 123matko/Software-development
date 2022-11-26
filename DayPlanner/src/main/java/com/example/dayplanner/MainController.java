a47f850983fa0a4acfff176aa1d2d026560d30b2
Martin Sopko <martin.sopko@student.tuke.sk>
2022-11-26 21:07:44 +0100

    main page layout genaration days are not on right place now

--- DayPlanner/src/main/java/com/example/dayplanner/MainController.java
+++ DayPlanner/src/main/java/com/example/dayplanner/MainController.java
@@ -8,26 +8,37 @@ import javafx.scene.layout.GridPane;
 import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
     @FXML
     private GridPane calendarPane;
-    private Button[] days ;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         Calendar rightNow = Calendar.getInstance();
-        for(int i=0;i<rightNow.getMaximum(Calendar.DAY_OF_MONTH)-1;i++){
+        rightNow.setFirstDayOfWeek(Calendar.MONDAY);
+        System.out.println(rightNow.getMaximum(Calendar.DAY_OF_MONTH));
+
+        for(int i=0;i<rightNow.getMaximum(Calendar.DAY_OF_MONTH);i++){
+            Button days = new Button(String.valueOf(i));
+            days.setStyle("-fx-border-color: #15BDD8");
+            Calendar thatDay = new GregorianCalendar(rightNow.get(Calendar.YEAR),rightNow.get(Calendar.MONTH),i+1);
+            int day = thatDay.get(Calendar.DAY_OF_WEEK);
+            int weak = thatDay.get(Calendar.WEEK_OF_MONTH);
+            System.out.println(i+","+day+","+weak);
+            if(day!=1) {
+                calendarPane.add(days, day-2, weak);
+            }else {
+                calendarPane.add(days, 6, weak);
+            }
 
         }
-        rightNow.setFirstDayOfWeek(Calendar.MONDAY);
-        Button sat = new Button(String.valueOf(rightNow.get(Calendar.DAY_OF_MONTH)));
-        int day = rightNow.get(Calendar.DAY_OF_WEEK);
-        int weak = rightNow.get(Calendar.WEEK_OF_MONTH);
-        if(day!=0) {
-            calendarPane.add(sat, day-1, weak);
-        }
+
+
 
 
 
