1dc8a9790ef5ae697fb0a1e4540958fb375a93e0
Martin Sopko <martin.sopko@student.tuke.sk>
2022-11-26 18:46:17 +0100

    main screen

--- DayPlanner/src/main/java/com/example/dayplanner/MainController.java
+++ DayPlanner/src/main/java/com/example/dayplanner/MainController.java
@@ -1,4 +1,35 @@
 package com.example.dayplanner;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private GridPane calendarPane;
+    private Button[] days ;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Calendar rightNow = Calendar.getInstance();
+        for(int i=0;i<rightNow.getMaximum(Calendar.DAY_OF_MONTH)-1;i++){
+
+        }
+        rightNow.setFirstDayOfWeek(Calendar.MONDAY);
+        Button sat = new Button(String.valueOf(rightNow.get(Calendar.DAY_OF_MONTH)));
+        int day = rightNow.get(Calendar.DAY_OF_WEEK);
+        int weak = rightNow.get(Calendar.WEEK_OF_MONTH);
+        if(day!=0) {
+            calendarPane.add(sat, day-1, weak);
+        }
+        System.out.println(days.length);
+
+
+    }
 }
